646ae9c6ad0752158beb66f8a05b66f4bf6b072f
Hetu <hetu@appsmith.com>
2024-11-25 05:17:29 +0000

    System generated commit, after creating a new branch: sdf

--- pages/Page1/jsobjects/JSObject3/JSObject3.js
+++ pages/Page1/jsobjects/JSObject3/JSObject3.js
@@ -1,11 +1,12 @@
 export default {
 	myVar1: [],
 	myVar2: {},
-	myFun1: () => {
-		//write code here
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
 	},
-	myFun2: async () => {
-		
-		//use async-await or promises
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
 	}
 }
